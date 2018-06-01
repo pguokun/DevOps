16491e95fb2540763c833af2b9ef2486f0414102
pguokun <pguokun@gmail.com>
2018-06-01 15:33:06 +0800

    change

--- GoProjects/src/example-1/example-1.go
+++ GoProjects/src/example-1/example-1.go
@@ -8,31 +8,12 @@ import (
 	"time"
 )
 
-func Exitfunc() {
-	fmt.Println("开始退出...")
-	fmt.Println("执行清理...")
-	fmt.Println("结束退出...")
-	os.Exit(0)
+func main() {
+	go Task()
+	Start()
 }
 
-func main() {
-	//创建监听退出chan
-	var c = make(chan os.Signal)
-	//监听指定信号 ctrl+c kill
-	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-
-	go func() {
-		for s := range c {
-			switch s {
-			case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
-				fmt.Println("退出", s)
-				Exitfunc()
-			default:
-				fmt.Println("other", s)
-			}
-		}
-	}()
-
+func Task() {
 	fmt.Println("进程启动...")
 	var sum = 0
 	for {
@@ -40,4 +21,29 @@ func main() {
 		fmt.Println("sum: ", sum)
 		time.Sleep(time.Second)
 	}
+}
+
+func Start() {
+	//创建监听退出chan
+	var c = make(chan os.Signal)
+
+	//创建监听退出chan
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	for s := range c {
+		switch s {
+		case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+			fmt.Println("退出", s)
+			Exitfunc()
+		default:
+			fmt.Println("other", s)
+		}
+	}
+}
+
+func Exitfunc() {
+	fmt.Println("开始退出...")
+	fmt.Println("执行清理...")
+	fmt.Println("结束退出...")
+	os.Exit(0)
 }
